425cd5b7bdceebe6247ae80b96ce5787901b4d95
Bruno Tenório <tenoriobn@gmail.com>
2023-07-21 15:02:03 -0300

    Primeiras estilizações no header

--- src/Components/Steps/Step/index.jsx
+++ src/Components/Steps/Step/index.jsx
@@ -3,7 +3,14 @@ import { NavLink } from 'react-router-dom';
 
 export default function Step( {children, to, styles} ) {
     return (
-        <NavLink className={({isActive}) => `${styles.header__nav__navlink} ${isActive ? styles.stepActive : ""}`} to={to}>
+        <NavLink 
+            className={
+                ({isActive}) => `
+                    ${styles.header__nav__navlink} 
+                    ${isActive ? styles.header__nav__navlink__number_active : ""}
+                `} 
+            to={to}
+        >
             {children}
         </NavLink>
     )
--- src/Components/Steps/index.jsx
+++ src/Components/Steps/index.jsx
@@ -6,10 +6,25 @@ export default function Steps() {
     return (
         <header className={styles.header}>
             <nav className={styles.header__nav}>
-                <Step to="/" styles={styles}><span className={styles.number}>1</span> <span className={styles.text}>Your Info</span></Step>
-                <Step to="/selectplan" styles={styles}><span className={styles.number}>2</span> <span className={styles.text}>Select Plan</span></Step>
-                <Step to="/pickaddons" styles={styles}><span className={styles.number}>3</span> <span className={styles.text}>Add-Ons</span></Step>
-                <Step to="/finishingup" styles={styles}><span className={styles.number}>4</span> <span className={styles.text}>Summary</span></Step>
+                <Step to="/" styles={styles}>
+                    <span className={styles.header__nav__navlink__number}>1</span> 
+                    <span className={styles.header__nav__navlink__text}>Your Info</span>
+                </Step>
+                
+                <Step to="/selectplan" styles={styles}>
+                    <span className={styles.header__nav__navlink__number}>2</span> 
+                    <span className={styles.header__nav__navlink__text}>Select Plan</span>
+                </Step>
+
+                <Step to="/pickaddons" styles={styles}>
+                    <span className={styles.header__nav__navlink__number}>3</span> 
+                    <span className={styles.header__nav__navlink__text}>Add-Ons</span>
+                </Step>
+                
+                <Step to="/finishingup" styles={styles}>
+                    <span className={styles.header__nav__navlink__number}>4</span> 
+                    <span className={styles.header__nav__navlink__text}>Summary</span>
+                </Step>
             </nav>
         </header>
     )
