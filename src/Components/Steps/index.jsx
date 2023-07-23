7900ae0d9fb99d85bd9742ef382e350ec2c395e2
Bruno Tenório <tenoriobn@gmail.com>
2023-07-23 10:27:27 -0300

    Utilizando json para tornar o menu dinâmico

--- src/Components/Steps/index.jsx
+++ src/Components/Steps/index.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import styles from './Steps.module.scss';
 import Step from './Step';
+import steps from './steps.json'
 
 export default function Steps() {
     return (
         <header className={styles.header}>
             <nav className={styles.nav}>
-                <li className={styles.li}>
+                {steps.map((step) => (
+                    <li key={step.id} className={styles.li}>
+                        <Step to={step.to} styles={styles}>{step.id}</Step>
+
+                        <div className={styles.step__section}>
+                            <h4 className={styles.step__number}>{step.number}</h4>
+                            <h3 className={styles.step__name}>{step.name}</h3>
+                        </div>
+                    </li>
+                ))}
+            </nav>
+        </header>
+    )
+}
+
+
+/*
+
+            <nav className={styles.nav}>
+                <li key={1} className={styles.li}>
                     <Step to="/" styles={styles}>1</Step>
 
                     <div className={styles.step__section}>
@@ -42,6 +62,5 @@ export default function Steps() {
                     </div>
                 </li>
             </nav>
-        </header>
-    )
-}
+
+*/
