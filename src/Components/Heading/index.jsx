93fb1e303cd68655040859c05008fe3e8c3c9e56
Bruno Tenório <tenoriobn@gmail.com>
2023-07-30 12:10:11 -0300

    Criando componente ThankYou

--- src/Components/Heading/index.jsx
+++ src/Components/Heading/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import styles from './Heading.module.scss'
 
-export default function Heading( {title, paragraph} ) {
+export default function Heading( {title, paragraph, thankyouparagraph} ) {
     return (
         <>
             <h2 className={styles.title}>{title}</h2>
-            <h3 className={styles.paragraph}>{paragraph}</h3>
+            <h3 className={`${styles.paragraph} ${styles[thankyouparagraph]}`}>{paragraph}</h3>
         </>
     )
 }
--- src/Components/ThankYou/index.jsx
+++ src/Components/ThankYou/index.jsx
@@ -1,19 +1,20 @@
-// import React from 'react';
-// import styles from './ThankYou.module.scss';
-// import Heading from 'Components/Heading';
-// import iconThankYou from './icon-thank-you.svg';
+import React from 'react';
+import styles from './ThankYou.module.scss';
+import Heading from 'Components/Heading';
+import iconThankYou from './icon-thank-you.svg';
 
-// export default function ThankYou() {
-//     return (
-//         <section className={styles.thanksyou__container}>
-//             <img src={iconThankYou} alt="thank you icon" className={styles.icon} />
+export default function ThankYou() {
+    return (
+        <section className={styles.thanksyou__container}>
+            <img src={iconThankYou} alt="thank you icon" className={styles.icon} />
 
-//             <Heading
-//                 title="Thank you!"
-//                 paragraph="Thanks for confirming your subscription! We hope you have fun 
-//                             using our platform. If you ever need support, please feel free 
-//                             to email us at support@loremgaming.com."
-//             />
-//         </section>
-//     )
-// }
+            <Heading 
+                thankyouparagraph="thankyou__paragraph"
+                title="Thank you!"
+                paragraph="Thanks for confirming your subscription! We hope you have fun 
+                            using our platform. If you ever need support, please feel free 
+                            to email us at support@loremgaming.com."
+            />
+        </section>
+    )
+}
