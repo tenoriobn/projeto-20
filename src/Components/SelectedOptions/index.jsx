f5a2fe2e7d2294231981454c94b634cf41e25be0
Bruno Tenório <tenoriobn@gmail.com>
2023-08-05 09:39:02 -0300

    Alternando tags do componente SelectedOptions

--- src/Components/SelectedOptions/index.jsx
+++ src/Components/SelectedOptions/index.jsx
@@ -5,48 +5,32 @@ export default function SelectedOptions() {
     return (
         <>
             <div className={styles.finishingup}>
-                <div className={styles.finishingup__container}>
+                <article className={styles.finishingup__container}>
                     <div className={styles.selected__container}>
                         <h3 className={styles.plan}>Arcade (Monthly)</h3>
-                        <h4 className={styles.change}>
-                            Change
-                        </h4>
+                        <h4 className={styles.change}>Change</h4>
                     </div>
 
-                    <h5 className={styles.plan__value}>
-                        $9/mo
-                    </h5>
-                </div>
+                    <h5 className={styles.plan__value}>$9/mo</h5>
+                </article>
 
-                <div className={styles.line}></div>
+                <span className={styles.line}></span>
 
-                <div className={styles.finishingup__container}>
-                    <h4 className={styles.plan__info}>
-                        Online service
-                    </h4>
-                    <h5 className={styles.addons__value}>
-                        +$1/mo
-                    </h5>
-                </div>
+                <article className={styles.finishingup__container}>
+                    <h4 className={styles.plan__info}>Online service</h4>
+                    <h5 className={styles.addons__value}>+$1/mo</h5>
+                </article>
 
-                <div className={styles.finishingup__container}>
-                    <h4 className={styles.plan__info}>
-                        Larger storage
-                    </h4>
-                    <h5 className={styles.addons__value}>
-                        +$2/mo
-                    </h5>
-                </div>
+                <article className={styles.finishingup__container}>
+                    <h4 className={styles.plan__info}>Larger storage</h4>
+                    <h5 className={styles.addons__value}>+$2/mo</h5>
+                </article>
             </div>
 
-            <div className={styles.total__container}>
-                <h4 className={styles.plan__info}>
-                    Total (per month)
-                </h4>
-                <h5 className={styles.addons__value}>
-                    +$12/mo
-                </h5>
-            </div>
+            <article className={styles.total__container}>
+                <h4 className={styles.plan__info}>Total (per month)</h4>
+                <h5 className={styles.addons__value}>+$12/mo</h5>
+            </article>
         </>
     )
 }
