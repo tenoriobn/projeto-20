72c56d188b0cdbdaf9d2c10c247e884ae417aa4b
Bruno Tenório <tenoriobn@gmail.com>
2023-08-21 11:27:31 -0300

    Pegando dados dos planos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import AppRoutes from 'Pages/routes/routes';
 import './styles/globalStyles.scss';
-import AppLayout from 'Pages/routes/routes';
 
 export default function App() {
   return (
--- src/Components/CardPlan/index.jsx
+++ src/Components/CardPlan/index.jsx
@@ -4,10 +4,16 @@ import plans from './plans.json';
 import Icons from './Icons';
 
 export default function CardPlan({ isYearly }) {
-    const [activePlan, setActivePlan] = useState(1);
+    const [activePlan, setActivePlan] = useState();
 
-    const selectedPlan = (planId) => {
-        setActivePlan(planId);
+    const formatPlanValue = (value, isYearly) => {
+        return isYearly ? `${value * 10}/yr` : `${value}/mo`;
+    }
+
+    const selectedPlan = (plan) => {
+        setActivePlan(plan.id);
+
+        console.log(formatPlanValue(plan.planValue, isYearly));
     }
 
     return (
@@ -16,7 +22,7 @@ export default function CardPlan({ isYearly }) {
                 <article 
                     key={plan.id} 
                     className={`${styles.plan__card} ${activePlan === plan.id ? styles.plan_active : ''}`}
-                    onClick={() => selectedPlan(plan.id)}
+                    onClick={() => selectedPlan(plan)}
                 >
                     
                     <img src={Icons[plan.img]} alt={`${plan.planName} flat icon`} className={styles.plan__card__img} />
@@ -24,7 +30,7 @@ export default function CardPlan({ isYearly }) {
                     <div className={styles.container__card}>
                         <h4 className={styles.plan__name}>{plan.planName}</h4>
                         <h5 className={styles.plan__value}>$
-                            {isYearly ? plan.planValue * 10 + '/yr' : plan.planValue + '/mo'}
+                            {formatPlanValue(plan.planValue, isYearly)}
                         </h5>
 
                         {isYearly ? <p className={styles.plan__discount}>2 months free</p> : ''}
