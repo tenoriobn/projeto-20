1d5c08fbe2c80b14273e0aff690ce40ea31fd94e
Bruno Tenório <tenoriobn@gmail.com>
2023-09-23 11:28:14 -0300

    Refatorando e adicionando ul no componente Steps

--- src/Components/Steps/index.jsx
+++ src/Components/Steps/index.jsx
@@ -6,17 +6,19 @@ import steps from './steps.json'
 export default function Steps() {
     return (
         <header className={styles.header}>
-            <nav className={styles.nav}>
-                {steps.map((step) => (
-                    <li key={step.id} className={styles.li}>
-                        <Step to={step.to} styles={styles}>{step.id}</Step>
+            <nav className={styles.steps}>
+                <ul className={styles.list__steps}>
+                    {steps.map((step) => (
+                        <li key={step.id} className={styles.step__item}>
+                            <Step to={step.to} styles={styles}>{step.id}</Step>
 
-                        <div className={styles.step__section}>
-                            <h4 className={styles.step__number}>{step.number}</h4>
-                            <h3 className={styles.step__name}>{step.name}</h3>
-                        </div>
-                    </li>
-                ))}
+                            <div className={styles.step__section}>
+                                <h4 className={styles.step__number}>{step.number}</h4>
+                                <h3 className={styles.step__name}>{step.name}</h3>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
             </nav>
         </header>
     )
